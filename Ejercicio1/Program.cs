d4357f874fa982adcee248e5305dbd8496f2b915
AlpaKaZhino98 <gastonpelo17@gmail.com>
2022-06-18 09:54:26 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Con los conocimientos vistos hasta ahora en clase realizar un programa que haga lo siguiente:
+ */
+
+int longitud;
+
+//1)      Pedir al usuario la longitud de un vector
+
+Console.WriteLine("Hola usuario, por favor indique la longitud que desee para el vector");
+longitud = int.Parse(Console.ReadLine());
+
+
+//2)      Crear el vector del tamaño ingresado.
+int[] vector = new int[longitud];
+
+
+//3)      Llenar el mismo con datos aleatorios
+Random rd = new Random();
+
+for (int i=0; i<vector.Length; i++) {
+    vector[i] = rd.Next(1,101);
+}
+
+
+//4)      Mostrar el vector por pantalla
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("-------------------------------------");
+
+Console.WriteLine();
+Console.Write("Vector generado: ");
+foreach (int e in vector) {
+    Console.Write($"{e} ");
+}
+
+
+/*
+ 5)      Invertir el vector, de manera que el primer elemento quede al último y el útimo en el primero; 
+el segundo en anteúltimo, el anteúltimo en el segundo y así sucesivamente. 
+En otra palabras si el vector es de 5 posiciones y el usuario ingresó: 10, 20, 30, 40, 50, una vez invertido,
+el vector debe quedar así: 50, 40, 30, 20, 10.  
+Se debe usar solo lo visto en clase hasta ahora y no los métodos que trae C# para estas cuestiones.
+*/
+int aux = 0;
+for (int i=0; i<vector.Length/2; i++) {
+    aux = vector[i];
+    vector[i] = vector[vector.GetUpperBound(0) - i];
+    vector[vector.GetUpperBound(0)-i] = aux;
+}
+
+
+//6)      Mostrar el vector nuevamente.
+Console.WriteLine();
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("-------------------------------------");
+
+Console.WriteLine();
+Console.Write("Vector invertido: ");
+foreach (int e in vector) {
+    Console.Write($"{e} ");
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,182 @@
+﻿Console.Clear();
+
+for (int g=0; g<3; g++) // CANTIDAD DE CARTONES A MOSTRAR
+{ 
+    int filas = 3;
+    int columnas = 9;
+
+    var carton = new int[filas, columnas];
+
+    //CARGO DATOS ALEATORIOS POR CADA COLUMNA USANDO EXTREMOS
+
+    var rd = new Random();
+
+    int limiteInferior = 1;
+    int limiteSuperior = 10;
+    int offset = 10;
+
+    bool seRepite;
+    int aux;
+
+    for (int j=0; j<=carton.GetUpperBound(1); j++)  //COLUMNAS
+    {
+        for (int i=0; i<=carton.GetUpperBound(0); i++)  //FILAS
+        {
+            do {
+                seRepite = false;
+                aux = rd.Next(limiteInferior, limiteSuperior);
+                
+                for (int h = 0; h < i; h++)
+                {
+                    if (carton[h, j] == aux)
+                    {
+                        aux = rd.Next(limiteInferior, limiteSuperior);
+                        seRepite = true;
+                    }
+                }
+                
+            } while (seRepite);
+
+            carton[i, j] = aux;
+        }
+
+        if (j == carton.GetUpperBound(1)-1) limiteSuperior++;   //En la fila 8 le sumo 1 extra para que en la sig llegue a 90
+        limiteInferior += offset;
+        limiteSuperior += offset;
+    }
+
+    //ORDENO LAS COLUMNAS
+
+    int aux2;
+
+    for (int j=0; j<=carton.GetUpperBound(1); j++)  //COLUMNAS
+    {
+        for (int i=0; i<carton.GetUpperBound(0); i++)  //FILAS
+        {
+            for (int h=0; h<carton.GetUpperBound(0); h++)
+            {
+                if (carton[h,j]>carton[h+1,j])
+                {
+                    aux2 = carton[h, j];
+                    carton[h, j] = carton[h + 1, j];
+                    carton[h + 1, j] = aux2;
+                }
+            }
+        }
+    }
+
+    // GENERO ESPACIOS ALEATORIOS PARA LA FILA 1
+
+    int aux3;
+    int contadorEspaciosVacios = 0;
+
+    bool repetido;
+
+    do
+    {
+        do
+        {
+            aux3 = rd.Next(1,10)-1; //resto 1 porque es para posicion del array   
+            repetido = true;
+
+            if (carton[0, aux3] != 0)
+            {
+                repetido = false;
+            }
+        } while (repetido);
+       
+        carton[0, aux3] = 0;
+        contadorEspaciosVacios++;
+    } while (contadorEspaciosVacios<4);
+
+    // GENERO ESPACIOS ALEATORIOS PARA LA FILA 2 
+    // SI EN LA POSICION ANTERIOR YA HAY UN ESPACIO VACIO TIENE MENOS CHANCE DE VOLVER A SALIR
+
+    contadorEspaciosVacios = 0;
+
+    do
+    {
+        do
+        {
+            aux3 = rd.Next(1, 10) - 1; //resto 1 porque es para posicion del array   
+            repetido = true;
+
+            if (carton[1, aux3] != 0)
+            {
+                //Con este if le quito probabilidad de repetir otro espacio vacio en la misma columna
+                if (carton[0, aux3] == 0)
+                {
+                    aux2 = rd.Next(0,100);
+                    repetido = true;
+                    
+                    if (aux2 == 50)
+                    {
+                        repetido = false;
+                    }
+                }
+                else
+                {                    
+                    repetido = false;
+                }
+            }
+        } while (repetido);
+
+        carton[1, aux3] = 0;
+        contadorEspaciosVacios++;
+    } while (contadorEspaciosVacios < 4);
+
+    // GENERO ESPACIOS ALEATORIOS PARA LA FILA 3
+    //PARA ESTE CASO PRIORIZO LAS COLUMNAS DONDE NO HALLAN ESPACIOS VACIOS TODAVIA!
+    //FUERZO EL ESPACIO EN BLANCO EN ESE CASO
+
+    contadorEspaciosVacios = 0;
+
+    for (int j=0; j<=carton.GetUpperBound(1); j++)  //COLUMNA 
+    {
+        if (carton[0,j]!=0 && carton[1,j]!=0)
+        {
+            carton[2, j] = 0;
+            contadorEspaciosVacios++;
+        }
+    }
+
+    //En este caso no usare probabilidad de aparicion para no alargar el codigo
+    //Con tener en cuenta no poner un espacio vacio donde ya  halla dos es suficiente para la mayoria de casos posibles
+    do
+    {
+        do
+        {
+            aux3 = rd.Next(1, 10) - 1; //resto 1 porque es para posicion del array   
+            repetido = false;
+
+            if ((carton[1, aux3] == 0 && carton[0,aux3]==0) || carton[2,aux3]==0)  //NO PUEDE HABER 3 ESPACIOS VACIOS
+            {
+                repetido = true;
+            }
+        } while (repetido);
+
+        carton[2, aux3] = 0;
+        contadorEspaciosVacios++;
+    } while (contadorEspaciosVacios < 4);
+
+    //MUESTRO LOS DATOS
+
+    Console.WriteLine("\t\t    --- BINGO ---");
+    Console.WriteLine("------------------------------------------------------");
+    for (int i=0; i<=carton.GetUpperBound(0); i++)  //FILAS
+    {
+        for (int j = 0; j<=carton.GetUpperBound(1) ; j++)   //COLUMNAS  
+        {
+            if (carton[i, j] == 0)
+            {
+                Console.Write("|    |");
+            }
+            else
+            {
+                Console.Write($"| {carton[i, j]:00} |");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("------------------------------------------------------");
+    }
+}
